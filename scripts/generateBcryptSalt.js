4db3c65be977e4d6efcecb89ce06a23b5445ae19
rootcause0 <caracobiceps@yandex.com>
2022-09-22 21:56:03 +0300

    [feature] dotenv and bcrypt salt generation script

---
+++ scripts/generateBcryptSalt.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt')
+const fs = require('fs')
+const os = require('os')
+
+const setEnvValue = (key, value) => {
+
+    // read file from hdd & split if from a linebreak to a array
+    const ENV_VARS = fs.readFileSync("./.env", "utf8").split(os.EOL);
+
+    // find the env we want based on the key
+    const target = ENV_VARS.indexOf(ENV_VARS.find((line) => {
+        return line.match(new RegExp(key));
+    }));
+
+    // replace the key/value with the new value
+    ENV_VARS.splice(target, 1, `${key}=${value}`);
+
+    // write everything back to the file system
+    fs.writeFileSync("./.env", ENV_VARS.join(os.EOL));
+
+}
+
+
+
+const salt = bcrypt.genSaltSync(10);
+setEnvValue('BCRYPT_SALT',salt)
